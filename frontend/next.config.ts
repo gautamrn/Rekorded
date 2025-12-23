93127d9ca8c8c32659a9f237512c6d4bb4cc7070
gautamrn <gautamrn@umich.edu>
2025-12-23 11:23:25 -0500

    new day new vercel fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
